a087bff4e6dc96e13683be14f1f284b82871b774
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-05 00:14:32 -0300

    2-commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D physical;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+    }
+
+    private void Awake()
+    {
+        this.physical = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.GetUp();
+        }
+    }
+
+    private void GetUp()
+    {
+        this.physical.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
     }
 }
