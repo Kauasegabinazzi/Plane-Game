14aae81592a3e4270f0ed7485d6ecbcd78adc1f6
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-08 22:57:12 -0300

    8-commit

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,6 +4,14 @@ public class Obstacle : MonoBehaviour
 {
     [SerializeField]
     private float speed = 0.1f;
+    [SerializeField]
+    private float variationY;
+
+    private void Awake()
+    {
+        transform.Translate(Vector3.up * Random.Range(-variationY, variationY));
+    }
+
     // Update is called once per frame
     void Update()
     {
