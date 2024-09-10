5819833057a8de7460e497df4ae944b6862b31b8
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-09 22:22:27 -0300

    12-commit

--- Assets/Scripts/Director.cs
+++ Assets/Scripts/Director.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Director : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject imageGameOver;
+
     public void FinishGame()
     {
         Time.timeScale = 0;
+        imageGameOver.SetActive(true);
     }
 }
