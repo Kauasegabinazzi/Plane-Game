f8359515ffbb4f8674cc98f0df1b95787502e713
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-16 22:05:29 -0300

    23-commit

--- Assets/Scripts/Difficulty.cs
+++ Assets/Scripts/Difficulty.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class difficulty : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float timeForDificultyMax;
+    private float timePassed;
+    private float dificulty;
 
     // Update is called once per frame
     void Update()
     {
-        
+        timePassed += Time.deltaTime;
+        dificulty = timePassed / timeForDificultyMax;
+        dificulty = Mathf.Min(1, dificulty);
+        Debug.Log(dificulty);
     }
 }
